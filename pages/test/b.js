cf0144cf070355e56e3cbd7a6544e1ef152bf0cd
Xuetao Han <haxneutao0917@live.com>
2020-03-18 18:15:10 +0200

    new react hook studying

--- pages/test/b.js
+++ pages/test/b.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Button} from "antd";
 
 class MyCount extends React.Component {
     state = {
         count: 0
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({ count: this.state.count + 1})
+        },1000)
+    }
+
+    componentWillUnmount() {
+        if(this.interval){
+            clearInterval(this.interval)
+        }
     }
 
     render() {
@@ -12,4 +24,19 @@ class MyCount extends React.Component {
         )
     }
 }
-export default () => <Button>Page B</Button>
+
+function MyCountFunc() {
+    const [count, setCount] = useState(0) //Array Destructuring Assignment [a,b] = 0
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount(count => count + 1)
+        },1000)
+
+        return () => clearInterval(interval)
+    }, [])
+
+    return <span>{count}</span>
+}
+
+export default MyCountFunc
